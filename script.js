a21e4e3d4d31c28f7cee9f3ca88a97876ec6da49
Deepak yadav <kirardivyansh9@gmail.com>
2024-09-08 23:57:52 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('contact-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Thank you for your message. I will get back to you soon!');
+    // Here you can add code to send form data to your server or email
+});
